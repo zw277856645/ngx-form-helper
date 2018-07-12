6a5d9ed6ef031bf9be7c88c64d8b1028e55d3aa0
teame_11 <teame_11@ele.me>
2018-07-12 13:19:45 +0800

    add

--- public_api.ts
+++ public_api.ts
@@ -1,2 +1,9 @@
 export * from './src/form-helper.module';
+export * from './src/form-helper-config';
 export * from './src/async-validator-limit';
+
+export * from './src/error-handler/error-handler-tooltip';
+export * from './src/error-handler/error-handler-tooltip-config';
+
+export * from './src/submit-handler/submit-handler';
+export * from './src/submit-handler/submit-handler-loader-config';
