07dee6db8690b16762a12cbee4d5af1432670027
teame_11 <teame_11@ele.me>
2020-01-07 15:29:29 +0800

    doc update

--- src/form-helper.directive.ts
+++ src/form-helper.directive.ts
@@ -42,7 +42,6 @@ export const FORM_HELPER_CONFIG_ARRAY = new InjectionToken<FormHelperConfig[]>('
  *     ...
  *     providers: [
  *         formHelperConfigProvider({
- *             autoReset: false,
                offsetTop: 100
  *             ...
  *         })
